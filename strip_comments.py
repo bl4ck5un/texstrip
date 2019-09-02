66568183a4ab0f8a701cde9a936ad062a7f238b7
Fan Zhang <bl4ck5unxx@gmail.com>
2019-09-02 17:18:24 -0400

    update

--- strip_comments.py
+++ strip_comments.py
@@ -1,106 +1,130 @@
 #!/usr/bin/env python
 
 
-import ply.lex, argparse, io
+import argparse
+import io
+import ply.lex
+import logging
 
-#Usage
+# Usage
 # python stripcomments.py input.tex > output.tex
 # python stripcomments.py input.tex -e encoding > output.tex
 
 def strip_comments(source):
     tokens = (
-                'PERCENT', 'BEGINCOMMENT', 'ENDCOMMENT', 'BACKSLASH',
-                'CHAR', 'BEGINVERBATIM', 'ENDVERBATIM', 'NEWLINE', 'ESCPCT',
-             )
+        'PERCENT', 'BEGINCOMMENT', 'ENDCOMMENT', 'BACKSLASH',
+        'CHAR', 'BEGINVERBATIM', 'ENDVERBATIM', 'NEWLINE', 'ESCPCT',
+    )
     states = (
-                ('linecomment', 'exclusive'),
-                ('commentenv', 'exclusive'),
-                ('verbatim', 'exclusive')
-            )
+        ('linecomment', 'exclusive'),
+        ('commentenv', 'exclusive'),
+        ('verbatim', 'exclusive')
+    )
 
-    #Deal with escaped backslashes, so we don't think they're escaping %.
-    def t_ANY_BACKSLASH(t):
+    # Deal with escaped backslashes, so we don't think they're escaping %.
+    def t_BACKSLASH(t):
         r"\\\\"
+        logging.debug('handling backslash at {}'.format(t.lexer.lineno))
         return t
 
-    #One-line comments
+    # One-line comments
     def t_PERCENT(t):
         r"\%"
         t.lexer.begin("linecomment")
 
-    #Escaped percent signs
+    # Escaped percent signs
     def t_ESCPCT(t):
         r"\\\%"
         return t
 
-    #Comment environment, as defined by verbatim package
+    # Comment environment, as defined by verbatim package
     def t_BEGINCOMMENT(t):
         r"\\begin\s*{\s*comment\s*}"
+        logging.debug('entering a comment environment at line {}'.format(t.lexer.lineno))
         t.lexer.begin("commentenv")
 
-    #Verbatim environment (different treatment of comments within)
+    # Verbatim environment (different treatment of comments within)
     def t_BEGINVERBATIM(t):
         r"\\begin\s*{\s*verbatim\s*}"
         t.lexer.begin("verbatim")
         return t
 
-    #Any other character in initial state we leave alone
+    # Any other character in initial state we leave alone
     def t_CHAR(t):
         r"."
         return t
 
     def t_NEWLINE(t):
-        r"\n"
+        r"\n+"
+        t.lexer.lineno += len(t.value)
         return t
 
-    #End comment environment
+    # End comment environment
     def t_commentenv_ENDCOMMENT(t):
         r"\\end\s*{\s*comment\s*}"
-        #Anything after \end{comment} on a line is ignored!
+        # Anything after \end{comment} on a line is ignored!
         t.lexer.begin('linecomment')
 
-    #Ignore comments of comment environment
+    # Ignore comments of comment environment
     def t_commentenv_CHAR(t):
         r"."
         pass
 
     def t_commentenv_NEWLINE(t):
-        r"\n"
-        pass
+        r"\n+"
+        t.lexer.lineno += len(t.value)
 
-    #End of verbatim environment
+    # End of verbatim environment
     def t_verbatim_ENDVERBATIM(t):
         r"\\end\s*{\s*verbatim\s*}"
         t.lexer.begin('INITIAL')
         return t
 
-    #Leave contents of verbatim environment alone
+    # Leave contents of verbatim environment alone
     def t_verbatim_CHAR(t):
         r"."
         return t
 
     def t_verbatim_NEWLINE(t):
-        r"\n"
+        r"\n+"
+        t.lexer.lineno += len(t.value)
         return t
 
-    #End a % comment when we get to a new line
+    # End a % comment when we get to a new line
     def t_linecomment_ENDCOMMENT(t):
         r"\n"
+        t.lexer.lineno += len(t.value)
         t.lexer.begin("INITIAL")
-        #Newline at the end of a line comment is stripped.
+        # Newline at the end of a line comment is stripped.
 
-    #Ignore anything after a % on a line
+    # Ignore anything after a % on a line
     def t_linecomment_CHAR(t):
         r"."
         pass
 
     lexer = ply.lex.lex()
     lexer.input(source)
+
+    # while True:
+    #     tok = lexer.token()
+    #     if not tok:
+    #         break  # No more input
+    #     print("({}, {}, {})".format(tok.type, repr(tok.value), tok.lineno, tok.lexpos))
+
     return u"".join([tok.value for tok in lexer])
+
+
+def strip_comments_from_files(infile, outfile):
+    with io.open(infile, encoding='utf-8') as f:
+        source = f.read()
+
+    with io.open(outfile, mode='w', encoding='utf-8') as f:
+        f.write(strip_comments(source))
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', help = 'the file to strip comments from')
+    parser.add_argument('filename', help='the file to strip comments from')
     parser.add_argument('--encoding', '-e', default='utf-8')
     parser.add_argument('--output', '-o', default='stripped.tex')
 
@@ -111,6 +135,7 @@ def main():
 
     with io.open(args.output, mode='w', encoding=args.encoding) as f:
         f.write(strip_comments(source))
+
 
 if __name__ == '__main__':
     main()
---
+++ texstrip.py
@@ -0,0 +1,68 @@
+"""
+Sanitize LaTeX sources for submission. This script removes comments
+(both inline ones (i.e., things after %) and comment environments),
+copies the supplementary files to a new folder for a clean submission.
+
+Usage:
+  texstrip.py [options] <main> [<extra> ...]
+
+Options:
+  -h --help                 Show this screen.
+  --version                 Show version.
+  --comments
+  --outdir=<outdir>         The output directory (relative to the main file) [default: stripped].
+  -b,--build                   How to build.
+  -c,--check                   Check the comments.
+"""
+
+from docopt import docopt
+import shutil
+from distutils import dir_util
+from strip_comments import strip_comments_from_files
+import logging
+import os
+import tempfile
+import subprocess
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    args = docopt(__doc__, version='texstrip v0.0.1')
+
+    main_file = args['<main>']
+    logging.debug('using {} as the main file'.format(main_file))
+
+    # extract target dir
+    output_dir = os.path.join(os.getcwd(), args['--outdir'])
+    os.makedirs(output_dir, exist_ok=True)
+
+    logging.debug("using {} as the output dir".format(output_dir))
+
+    # expand
+    target_main_file = os.path.join(output_dir, os.path.basename(main_file))
+    if target_main_file == main_file:
+        raise Exception('target main file is the same as the source')
+
+    cmd = 'latexpand --empty-comments -o {} {}'.format(os.path.join(output_dir, 'expanded.tex'), main_file)
+    subprocess.run(cmd, shell=True, check=True)
+    logging.info('Finished: {}'.format(cmd))
+
+    if args['<extra>']:
+        cp_cmd = "cp -rf {} {}".format(" ".join(args['<extra>']), output_dir)
+        subprocess.run(cp_cmd, shell=True, check=True)
+        logging.info("Finished: {}".format(cp_cmd))
+
+    # 2nd step: strip comments
+    strip_comments_from_files(os.path.join(output_dir, 'expanded.tex'),
+                              os.path.join(output_dir, 'stripped.tex'))
+
+    # 3rd step: copy
+    shutil.copyfile(os.path.join(output_dir, 'stripped.tex'), target_main_file)
+
+    if args['--build']:
+        os.chdir(output_dir)
+        build_cmd = "latexmk -pdf {}".format(target_main_file)
+        subprocess.run(build_cmd, shell=True, check=True)
+
+
+
+
